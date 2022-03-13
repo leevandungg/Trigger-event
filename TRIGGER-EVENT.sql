5b4598b2426d376df8c94601b021a61d16435e23
leevandungg <95156730+leevandungg@users.noreply.github.com>
2022-03-13 10:51:19 +0700

    LeVanDung

--- TRIGGER-EVENT.sql
+++ TRIGGER-EVENT.sql
@@ -1,13 +1,16 @@
 ﻿-- Trigger of DŨNG : Khi mua một đơn hàng nào đó thì số lượng đặt hàng nhỏ hơn hoặc bằng số lượng hiện có và tự động tính tiền
 CREATE TRIGGER trg_Check_Order ON CHITIETHOADON FOR INSERT, UPDATE AS
 BEGIN
-     DECLARE @SoLuongMua int, @SoLuongHienCo int, @GiaSP int, @MaSP NVARCHAR(50), @TongTien int, @MaDH NVARCHAR(50), @SoLuongDaMua int, @MaCT NVARCHAR(50)
+     DECLARE @SoLuongMua int, @SoLuongHienCo int, @GiaSP int, @MaSP NVARCHAR(50), @TongTien int, @MaDH NVARCHAR(50),
+	 @SoLuongDaMua int, @MaCT NVARCHAR(50), @PTTT NVARCHAR(100), @TienPhi int
 
 	 SELECT @SoLuongMua = SoLuong, @MaSP = MaSP, @TongTien = ThanhTien, @MaDH = MaDH, @MaCT = MaCTHD FROM inserted
 
 	 SELECT @SoLuongDaMua = SoLuong FROM deleted WHERE MaCTHD = @MaCT
 
-	 PRINT CAST(@SoLuongDaMua as varchar(50))
+	 SELECT @PTTT = PTTT FROM DONHANG WHERE MaDH = @MaDH
+
+	 SELECT @TienPhi = PhiTT FROM PAYMENT WHERE MaPTTT = @PTTT
 
 	 SELECT @SoLuongHienCo = SoluongSP, @GiaSP = GiaSP FROM PRODUCT WHERE MaSP = @MaSP
 
@@ -19,13 +22,15 @@ BEGIN
 	 ELSE IF(@TongTien != @GiaSP * @SoLuongMua)
 	 BEGIN
 	    UPDATE CHITIETHOADON SET ThanhTien = @GiaSP * @SoLuongMua WHERE MaCTHD = @MaCT
-		UPDATE DONHANG SET TongTien = @GiaSP * @SoLuongMua WHERE MaDH = @MaDH
+		UPDATE DONHANG SET TongTien = (@GiaSP * @SoLuongMua) + @TienPhi WHERE MaDH = @MaDH
 	 END
 END
 
 GO
 
-INSERT INTO DONHANG VALUES('DH006','2022-03-08',N'Đang giao',0,'KH003','SP001')
+DELETE FROM DONHANG WHERE MaDH = 'DH006'
+
+INSERT INTO DONHANG VALUES('DH006','2022-03-08',N'Đang giao',0,'KH003','SP001','TT01')
 
 INSERT INTO CHITIETHOADON VALUES ('CT006',3,20000,0,'DH006','SP001')
 
